eaf8af18b2e40e46181d8789ffd8238f10f44c4e
khanZhomart <jtalgatulu@yandex.kz>
2024-02-14 11:13:28 +0600

    🚀 Initial commit

---
+++ src/main/java/kz/azure/ms/MsBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.azure.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBackendApplication.class, args);
+    }
+
+}
