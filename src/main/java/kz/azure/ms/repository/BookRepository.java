abf8626bdd8d12f10bfb36e55e57fe1bfca261c9
DumanBruh <dmarlambekov@gmail.com>
2024-02-18 00:22:36 +0600

    feature: added mongo db database, book collection

---
+++ src/main/java/kz/azure/ms/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package kz.azure.ms.repository;
+
+import kz.azure.ms.models.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends MongoRepository<Book, String> {
+}
