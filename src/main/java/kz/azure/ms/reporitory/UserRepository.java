746f0fa85c78e42f21ef1caf426e07f5e4b50d32
Zhomartkhan Talgatuly <71120576+khanZhomart@users.noreply.github.com>
2024-02-18 22:37:42 +0600

    Fix password field in user mapper (#15)
    * #3 MVC layer without controller
    
    * Fix password field in user mapper
    
    ---------
    
    Co-authored-by: MZBolatov <mzbolatov@beeline.kz>
    Co-authored-by: Steelov2 <107679956+Steelov2@users.noreply.github.com>

--- src/main/java/kz/azure/ms/reporitory/UserRepository.java
+++ src/main/java/kz/azure/ms/reporitory/UserRepository.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends MongoRepository<User, ObjectId> {
   Optional<User> findByFio(@Param("fio") String ctn);
   Optional<User> findByUsername(@Param("username") String ctn);
+  
 }
